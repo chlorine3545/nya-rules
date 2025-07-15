210c59c3e5ce82442fa23dc125554c93f0a2a61a
Chlorine <146422759+chlorine3545@users.noreply.github.com>
2025-07-15 18:25:44 +0800

    feat: add proxy.txt

--- scripts/keywords.py
+++ scripts/keywords.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     main(sys.argv[1])
 
 
-#? 用于生成 keywords.txt 规则，使用 DOMAIN-KEYWORD 代替多个 DOMAIN-SUFFIX 规则
-#? 我这里没有管过滤问题，这个交给 AdGuard Home 来处理吧
+# ? 用于生成 keywords.txt 规则，使用 DOMAIN-KEYWORD 代替多个 DOMAIN-SUFFIX 规则
+# ? 我这里没有管过滤问题，这个交给 AdGuard Home 来处理吧
